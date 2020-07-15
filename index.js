abc1a397b15aeed4b3c9def278b9f3a247fb3091
Chevreau-Maxime <67205419+Chevreau-Maxime@users.noreply.github.com>
2020-07-15 16:59:14 +0200

    fixed asteroid display

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ app.get('/', function(req, res){
 
 var ASTEROIDS = [];
 var LOBBY = [];
-var turnspeed = 0.05;
-var acceleration = 0.1;
-var slowdown = 0.9;
-var turnslow = 0.8;
+var turnspeed = 0.02;
+var acceleration = 0.05;
+var slowdown = 0.95;
+var turnslow = 0.9;
 
 
 io.on('connection', function(socket){
@@ -31,10 +31,6 @@ io.on('connection', function(socket){
     console.log("__Disconnect : " + LOBBY[index].name);
     LOBBY.splice(index, 1);
   });
-
-
-
-
 
 });
 
@@ -87,7 +83,7 @@ function game_step(){
     //angular :
     if (LOBBY[i].input.left){ LOBBY[i].ship.da -= turnspeed; }
     if (LOBBY[i].input.right){ LOBBY[i].ship.da += turnspeed; }
-    LOBBY[i].ship.da *= turnslow;
+    LOBBY[i].ship.da *= (Math.abs(LOBBY[i].ship.da) < 0.001) ? 0:turnslow;
     LOBBY[i].ship.a += LOBBY[i].ship.da;
     
     //velocity :
@@ -95,11 +91,12 @@ function game_step(){
       LOBBY[i].ship.dx += Math.cos(LOBBY[i].ship.a)*acceleration;
       LOBBY[i].ship.dy += Math.sin(LOBBY[i].ship.a)*acceleration;
     }
-    LOBBY[i].ship.dx *= (LOBBY[i].ship.dx < 0.05) ? 0:slowdown;
-    LOBBY[i].ship.dy *= (LOBBY[i].ship.dy < 0.05) ? 0:slowdown;
     LOBBY[i].ship.x += LOBBY[i].ship.dx;
     LOBBY[i].ship.y += LOBBY[i].ship.dy;
+    LOBBY[i].ship.dx *= (Math.abs(LOBBY[i].ship.dx) < 0.001) ? 0:slowdown;
+    LOBBY[i].ship.dy *= (Math.abs(LOBBY[i].ship.dy) < 0.001) ? 0:slowdown;
     
+    //console.log(JSON.stringify(LOBBY[i].ship));
 
     //send info
     game_send_info(i)
@@ -112,9 +109,9 @@ function game_send_info(index){
   //asteroids
   var package = {asteroids:[]};
   for (var i=0; i<ASTEROIDS.length; i++){
-    if (LOBBY[index].ship == undefined){console.log("WTFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF" + JSON.stringify(LOBBY));}
+    if (LOBBY[index] == undefined){console.log("WTFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF" + JSON.stringify(LOBBY));}
     if (distance(ASTEROIDS[i], LOBBY[index].ship) < 50 || true){
-      package.asteroids[package.asteroids.length] = ASTEROIDS[i];
+      package.asteroids[package.asteroids.length] = Object.create(ASTEROIDS[i]);
     }
   }
   //normalize coordinates for target ship
@@ -130,12 +127,12 @@ function game_init_ship(index){
   LOBBY[index].input = {left:0, up:0, right:0, down:0};
 }
 
-function game_init_asteroids(x=0, y=0, belt_radius=1000, nb=100){
+function game_init_asteroids(x=0, y=0, belt_radius=20, nb=30){
   ASTEROIDS = [];
   var rand_angle, rand_radius;
   for (var i=0; i<nb; i++){
     rand_angle = Math.random()*2*Math.PI;
-    rand_radius = belt_radius * (1 + (0.1*Math.random()));
+    rand_radius = belt_radius * (1 + (0.3*Math.random()));
     ASTEROIDS[i] = {x:Math.cos(rand_angle)*rand_radius + x, y:Math.sin(rand_angle)*rand_radius + y};
   }
   //console.log(JSON.stringify(ASTEROIDS));
